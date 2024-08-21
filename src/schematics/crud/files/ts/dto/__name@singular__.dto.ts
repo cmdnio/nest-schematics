31f1b0a189beeba50c857a5a51ac4637686c4ca3
Dmitry Zaets <dmitry.zaets@gmail.com>
2024-08-21 13:54:51 +0200

    feat: add separate update dto

--- src/schematics/crud/files/ts/dto/__name@singular__.dto.ts
+++ src/schematics/crud/files/ts/dto/__name@singular__.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/swagger'
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
 
@@ -6,6 +5,10 @@ export const Create<%= singular(classify(name)) %>Schema = z.object({
   name: z.string().min(1).max(255)
 })
 
+export const Update<%= singular(classify(name)) %>Schema = z.object({
+  name: z.string().min(1).max(255)
+})
+
 export class Create<%= singular(classify(name)) %>Dto extends createZodDto(Create<%= singular(classify(name)) %>Schema) {}
-export class Update<%= singular(classify(name)) %>Dto extends PartialType(Create<%= singular(classify(name)) %>Dto) {}
+export class Update<%= singular(classify(name)) %>Dto extends createZodDto(Update<%= singular(classify(name)) %>Schema) {}
 
